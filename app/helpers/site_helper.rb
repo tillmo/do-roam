89966a2e7f6602ac99f8986ffebb5d2633f865c5
Till Mossakowski <till@communtu.de>
2011-02-10 15:10:31 +0100

    better menus

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,20 @@
 module SiteHelper
+  
+  def diplay_class_menu(c)
+    checkbox = check_box_tag("class[" + c.id.to_s + "]")
+    out = "<li><a>#{checkbox} #{c.name}"
+    if !c.subclasses.empty? then
+      out += "&nbsp;"
+      out += image_tag("right-arrow.png")
+    end
+    if !c.subclasses.empty?
+      out += "</a><ul>"
+      c.subclasses.each do |sub|
+        out += diplay_class_menu(sub)
+      end
+      out += "</ul>"
+    end
+    out += "</li>"
+    return out
+  end
 end
